8044a2d009ffabda4c799419bd439222d15c5323
jackyrusly <jackyrusly@gmail.com>
2021-07-21 22:31:16 +0700

    Describe

--- src/core/text/Text.test.tsx
+++ src/core/text/Text.test.tsx
@@ -1,12 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Text from './index';
 
-it('Render Text component correctly', () => {
-  const text = 'Text Component';
+describe('Text', () => {
+  it('Render Text component correctly', () => {
+    const text = 'Text Component';
 
-  render(<Text>{text}</Text>);
+    render(<Text>{text}</Text>);
 
-  const textElement = screen.getByText(text);
+    const textElement = screen.getByText(text);
 
-  expect(textElement).toBeInTheDocument();
+    expect(textElement).toBeInTheDocument();
+  });
 });
