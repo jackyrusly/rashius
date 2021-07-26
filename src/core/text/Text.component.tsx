66f001e354265830cd2ad30e817eaa146eb5a6d1
Jacky Rusly <jackyrusly@gmail.com>
2021-07-26 14:51:50 +0700

    Remove memo

--- src/core/text/Text.component.tsx
+++ src/core/text/Text.component.tsx
@@ -1,20 +1,13 @@
-import { FC, memo } from 'react';
+import { FC } from 'react';
 import IText from './Text.type';
 import StyledText from './Text.style';
 
-export const Text: FC<IText> = memo(
-  ({ children, $color = 'primary', $size = 'md' }) => {
-    return (
-      <StyledText $color={$color} $size={$size}>
-        {children}
-      </StyledText>
-    );
-  },
-);
-
-Text.defaultProps = {
-  $color: 'primary',
-  $size: 'md',
+export const Text: FC<IText> = ({ children, $color = 'primary', $size = 'md' }) => {
+  return (
+    <StyledText $color={$color} $size={$size}>
+      {children}
+    </StyledText>
+  );
 };
 
 export default Text;
