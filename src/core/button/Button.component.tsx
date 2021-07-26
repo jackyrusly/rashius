f08e2f6d67d708ccce04985bb365f24aaca08bfa
Jacky Rusly <jackyrusly@gmail.com>
2021-07-26 14:56:47 +0700

    Remove memo

--- src/core/button/Button.component.tsx
+++ src/core/button/Button.component.tsx
@@ -1,20 +1,13 @@
-import { FC, memo } from 'react';
+import { FC } from 'react';
 import IButton from './Button.type';
 import StyledButton from './Button.style';
 
-export const Button: FC<IButton> = memo(
-  ({ children, $isLoading = false, $type = 'primary' }) => {
-    return (
-      <StyledButton $isLoading={$isLoading} $type={$type}>
-        {$isLoading ? 'Loading...' : children}
-      </StyledButton>
-    );
-  },
-);
-
-Button.defaultProps = {
-  $isLoading: false,
-  $type: 'primary',
+export const Button: FC<IButton> = ({ children, $isLoading = false, $type = 'primary' }) => {
+  return (
+    <StyledButton $isLoading={$isLoading} $type={$type}>
+      {$isLoading ? 'Loading...' : children}
+    </StyledButton>
+  );
 };
 
 export default Button;
